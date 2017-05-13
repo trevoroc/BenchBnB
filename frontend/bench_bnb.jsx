21ea6dadf648623c2999f19de05120c44a001185
Trevor OConnor <trevoroc22@gmail.com>
2017-05-12 17:06:11 -0700

    session APIUtil actions

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as APIUTIL from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.signup = APIUTIL.signup;
+  window.login = APIUTIL.login;
+  window.logout = APIUTIL.logout;
   ReactDOM.render(<h1>HauntedHouseBnB</h1>, root);
 });
